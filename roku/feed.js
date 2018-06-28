e722795ebc046e8e07130947c92b88bbce111bd0
notanewbie <computerchipcity@aol.com>
2018-06-28 18:50:50 -0400

    Create feed.js

---
+++ roku/feed.js
@@ -0,0 +1,31 @@
+{
+    "providerName": "FOX",
+    "lastUpdated": "2018-06-28T18:43:00-05:00",
+    "language": "en-US",
+    "tvSpecials": [
+        {
+  "id": "001",
+  "title": "Fox News Talk",
+  "content": {
+    {
+  "dateAdded": "2018-06-28T18:49:00-00:00",
+  "videos": [
+    {
+  "url": "https://fnurtmp-f.akamaihd.net/i/FNRADIO_1@92141/master.m3u8",
+  "quality": "HD",
+  "videoType": "HLS"
+}
+  ],
+  "duration": 4500
+}
+  },
+  "genres": [
+    "News",
+    "US News",
+    "Conservative",
+  ],
+  "thumbnail": "https://radio.foxnews.com/wp-content/uploads/2012/12/FOX-News-Talk-Radio-1.jpg",
+  "shortDescription": "Conservative opinion from Fox News radio hosts."
+}
+    ]
+}
